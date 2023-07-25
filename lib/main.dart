e272f46b3d729608682d8c9ad631a8332418b1d5
rintosaputro <saputrorinto@gmail.com>
2023-07-25 17:19:21 +0700

    add tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,45 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  final Color? colorTextAppbar = const Color.fromARGB(255, 0, 74, 62);
+  final Color? colorSelectedTab = const Color.fromARGB(255, 2, 132, 111);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+        home: DefaultTabController(
+      length: 3,
+      child: SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Vegetarian mania',
+                  style: TextStyle(color: colorTextAppbar)),
+              bottom: TabBar(
+                labelColor: colorSelectedTab,
+                unselectedLabelColor: colorTextAppbar,
+                indicatorColor: colorSelectedTab,
+                tabs: const [
+                  Tab(text: 'Vegetable'),
+                  Tab(text: 'Fruit'),
+                  Tab(text: 'Legumes')
+                ],
+              ),
+              backgroundColor: const Color.fromRGBO(203, 231, 194, 1),
+            ),
+            body: const TabBarView(
+              children: [
+                Center(
+                  child: Text('Sawi'),
+                ),
+                Center(
+                  child: Text('Nangka'),
+                ),
+                Center(
+                  child: Text('Kacang'),
+                )
+              ],
+            )),
       ),
-    );
+    ));
   }
 }
