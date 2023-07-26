5ea6a6f52eb067548accc70967cb78016677b51c
rintosaputro <saputrorinto@gmail.com>
2023-07-26 18:01:18 +0700

    feat: item & grid section

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vegetarian/model/items_vegetarian.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,60 +14,95 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+            scaffoldBackgroundColor: const Color.fromRGBO(245, 244, 242, 1)),
         home: DefaultTabController(
-      length: 3,
-      child: SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text('Vegetarian mania',
-                  style: TextStyle(color: colorTextAppbar)),
-              bottom: TabBar(
-                labelColor: colorSelectedTab,
-                unselectedLabelColor: colorTextAppbar,
-                indicatorColor: colorSelectedTab,
-                tabs: const [
-                  Tab(text: 'Vegetable'),
-                  Tab(text: 'Fruit'),
-                  Tab(text: 'Legumes')
-                ],
-              ),
-              backgroundColor: const Color.fromRGBO(203, 231, 194, 1),
-            ),
-            body: TabBarView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    children: <Widget>[
-                      Card(
-                        child: Column(
-                          children: <Widget>[
-                            AspectRatio(
-                              aspectRatio: 2 / 1,
-                              child: Image.asset('images/potatoes.png'),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(top: 8, bottom: 8),
-                              child: Text('Potato'),
-                            ),
-                          ],
-                        ),
-                      ),
+          length: 3,
+          child: SafeArea(
+            child: Scaffold(
+                appBar: AppBar(
+                  title: Text('Vegetarian mania',
+                      style: TextStyle(color: colorTextAppbar)),
+                  bottom: TabBar(
+                    labelColor: colorSelectedTab,
+                    unselectedLabelColor: colorTextAppbar,
+                    indicatorColor: colorSelectedTab,
+                    tabs: const [
+                      Tab(text: 'Vegetables'),
+                      Tab(text: 'Fruits'),
+                      Tab(text: 'Legumes')
                     ],
                   ),
+                  backgroundColor: const Color.fromRGBO(203, 231, 194, 1),
                 ),
-                const Center(
-                  child: Text('Nangka a'),
-                ),
-                const Center(
-                  child: Text('Kacang'),
-                )
-              ],
-            )),
+                body: TabBarView(
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10),
+                            itemCount: vegetables.length,
+                            itemBuilder: (context, index) {
+                              return CardItem(item: vegetables[index]);
+                            })),
+                    Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10),
+                            itemCount: fruits.length,
+                            itemBuilder: (context, index) {
+                              return CardItem(item: fruits[index]);
+                            })),
+                    const Center(
+                      child: Text('Kacangaaa'),
+                    )
+                  ],
+                )),
+          ),
+        ));
+  }
+}
+
+class CardItem extends StatelessWidget {
+  final ItemsVegetarian item;
+  const CardItem({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+              child: Image.asset(
+                item.imageAsset,
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      item.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(item.nutrition, style: const TextStyle(fontSize: 10)),
+                  ],
+                ))
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
---
+++ lib/model/items_vegetarian.dart
@@ -0,0 +1,85 @@
+class ItemsVegetarian {
+  String name;
+  String nutrition;
+  String imageAsset;
+  String priceInMarket;
+  String description;
+
+  ItemsVegetarian(
+      {required this.name,
+      required this.nutrition,
+      required this.imageAsset,
+      required this.priceInMarket,
+      required this.description});
+}
+
+var vegetables = [
+  ItemsVegetarian(
+      name: 'Brocolli',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/vegetables/brocolli.png',
+      priceInMarket: 'Rp10.000',
+      description:
+          'Broccoli is a highly nutritious cruciferous vegetable with a wealth of health benefits. It is packed with essential vitamins and minerals, making it a valuable addition to any diet. Broccoli is a rich source of vitamin C, which boosts the immune system and supports skin health. It is also high in vitamin K, contributing to bone health and blood clotting. Furthermore, broccoli provides vitamin A, crucial for vision and immune function, and folate, important for cell division and DNA synthesis. The fiber content aids in digestion and helps maintain bowel regularity. Broccoli contains various antioxidants, like sulforaphane and quercetin, which protect against oxidative stress and reduce the risk of chronic diseases. Its low-calorie and low-fat nature make it an excellent choice for weight management. Enjoyed steamed, roasted, stir-fried, or added to soups and salads, broccoli is a delicious and nutritious addition to a well-rounded diet.'),
+  ItemsVegetarian(
+      name: 'Cabbage',
+      nutrition: 'Vit C: 0.366 mg/g',
+      imageAsset: 'images/vegetables/cabbage.png',
+      priceInMarket: 'Rp10.000',
+      description:
+          'Cabbage is a highly nutritious and versatile vegetable that offers an array of health benefits. It is low in calories but rich in essential nutrients. Cabbage is an excellent source of vitamin C, providing a boost to the immune system and promoting healthy skin. It is also high in vitamin K, which plays a key role in bone health and blood clotting. Additionally, cabbage contains vitamin B6, which is essential for metabolism and brain function. The fiber content aids in digestion and helps with bowel regularity. Cabbage belongs to the cruciferous vegetable family, known for its high levels of antioxidants, such as sulforaphane and kaempferol, which help protect the body from oxidative stress and may reduce the risk of chronic diseases. Enjoyed raw in salads, steamed, sautéed, or fermented to make dishes like sauerkraut, cabbage is a nutritious and delicious addition to a healthy diet.'),
+  ItemsVegetarian(
+      name: 'Carrot',
+      nutrition: 'Vit A: 167.05 IU/g',
+      imageAsset: 'images/vegetables/carrot.png',
+      priceInMarket: 'Rp10.000',
+      description:
+          'Carrots are a nutrient-dense and vibrant orange vegetable with a host of health benefits. They are an excellent source of beta-carotene, a powerful antioxidant that the body converts into vitamin A, essential for vision, immune function, and healthy skin. Carrots also provide vitamin C, supporting the immune system and collagen production for healthy skin and connective tissues. The fiber in carrots aids in digestion and promotes bowel regularity. Additionally, they contain potassium, which contributes to heart health and helps regulate blood pressure. The antioxidants in carrots, such as lutein and zeaxanthin, are beneficial for eye health. With their crunchy texture and sweet flavor, carrots are a convenient and delicious snack when raw, and they add natural sweetness to various cooked dishes like stews, soups, and stir-fries. Incorporating carrots into your diet can be a simple and enjoyable way to boost your overall health and well-being.'),
+  ItemsVegetarian(
+      name: 'Mustard',
+      nutrition: 'Protein: 0.024 g/g',
+      imageAsset: 'images/vegetables/mustard.png',
+      priceInMarket: 'Rp10.000',
+      description:
+          'Mustard is a versatile condiment made from mustard seeds and has some potential health benefits. Mustard seeds are a good source of various nutrients, including protein, fiber, vitamin B6, and minerals such as calcium, magnesium, and potassium. The spice also contains compounds like glucosinolates, which have been associated with potential health-promoting properties. Some studies suggest that consuming mustard seeds or mustard products may help support digestion, reduce inflammation, and provide antioxidant benefits. However, it`s essential to note that the potential health benefits of mustard are mostly related to consuming it in moderation as a condiment rather than in large quantities. As with any food, individual dietary needs and preferences should be taken into consideration.'),
+  ItemsVegetarian(
+      name: 'Potato',
+      nutrition: 'Calories: 0.77 kcal/g',
+      imageAsset: 'images/vegetables/potatoes.png',
+      priceInMarket: 'Rp10.000',
+      description:
+          'Potatoes are a nutritious and versatile vegetable, providing a good source of essential nutrients. They are rich in complex carbohydrates, mainly in the form of starch, offering a steady release of energy. Additionally, potatoes contain dietary fiber, supporting healthy digestion and bowel regularity. They are a good source of vitamin C, an antioxidant that boosts the immune system and promotes healthy skin. Potatoes also provide potassium, which aids in maintaining proper heart function and regulating blood pressure. As part of a balanced diet, potatoes can contribute to overall health and well-being, offering a satisfying and nourishing addition to various meals when prepared in a healthy manner. However, portion control and cooking methods are crucial to maximize their nutritional benefits and avoid adding excess unhealthy fats.'),
+];
+
+var fruits = [
+  ItemsVegetarian(
+      name: 'Apple',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/fruits/apple.png',
+      priceInMarket: 'Rp10.000',
+      description: ''),
+  ItemsVegetarian(
+      name: 'Grapes',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/fruits/grapes.png',
+      priceInMarket: 'Rp10.000',
+      description: ''),
+  ItemsVegetarian(
+      name: 'Mango',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/fruits/mango.png',
+      priceInMarket: 'Rp10.000',
+      description: ''),
+  ItemsVegetarian(
+      name: 'Orange',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/fruits/orange.png',
+      priceInMarket: 'Rp10.000',
+      description: ''),
+  ItemsVegetarian(
+      name: 'Peach',
+      nutrition: 'Vit C: 0.89 mg/g',
+      imageAsset: 'images/fruits/peach.png',
+      priceInMarket: 'Rp10.000',
+      description: ''),
+];
