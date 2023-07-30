7783895e8584b28c31cb41992070ba42040de5a7
rintosaputro <saputrorinto@gmail.com>
2023-07-30 21:46:43 +0700

    fix(grid item): responsive grid item

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -27,45 +27,63 @@ class MainScreen extends StatelessWidget {
               ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10),
-                      itemCount: vegetables.length,
-                      itemBuilder: (context, index) {
-                        return CardItem(item: vegetables[index]);
-                      })),
-              Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10),
-                      itemCount: fruits.length,
-                      itemBuilder: (context, index) {
-                        return CardItem(item: fruits[index]);
-                      })),
-              Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10),
-                      itemCount: legumes.length,
-                      itemBuilder: (context, index) {
-                        return CardItem(item: legumes[index]);
-                      })),
-            ],
+          body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              double totalChildAspectRation =
+                  constraints.maxWidth > 395 ? 1.0 : (2 / 2.5);
+              int totalCrossAxisCount = 2;
+
+              if (constraints.maxWidth > 700) {
+                totalCrossAxisCount = 3;
+              }
+              if (constraints.maxWidth > 1000) {
+                totalCrossAxisCount = 4;
+              }
+
+              return TabBarView(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: totalCrossAxisCount,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                  childAspectRatio: totalChildAspectRation),
+                          itemCount: vegetables.length,
+                          itemBuilder: (context, index) {
+                            return CardItem(item: vegetables[index]);
+                          })),
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: totalCrossAxisCount,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                  childAspectRatio: totalChildAspectRation),
+                          itemCount: fruits.length,
+                          itemBuilder: (context, index) {
+                            return CardItem(item: fruits[index]);
+                          })),
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: totalCrossAxisCount,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                  childAspectRatio: totalChildAspectRation),
+                          itemCount: legumes.length,
+                          itemBuilder: (context, index) {
+                            return CardItem(item: legumes[index]);
+                          })),
+                ],
+              );
+            },
           )),
     );
   }
@@ -86,30 +104,33 @@ class CardItem extends StatelessWidget {
         elevation: 20,
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(13.0)),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 80,
-              child: Image.asset(
-                item.imageAsset,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+                child: Image.asset(
+                  item.imageAsset,
+                ),
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Text(
-                      item.name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(item.nutrition,
-                        style: const TextStyle(
-                            fontSize: 10,
-                            color: Color.fromARGB(255, 2, 132, 111))),
-                  ],
-                ))
-          ],
+              Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text(
+                        item.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(item.nutrition,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 10,
+                              color: Color.fromARGB(255, 2, 132, 111))),
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
